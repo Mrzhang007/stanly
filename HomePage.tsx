c171f9bfae14e26bda5eaed2966d675711b822d3
zhangzhong <zhong.zhang@duom.com>
2023-04-24 14:29:43 +0800

    fix_home_bug01

--- HomePage.tsx
+++ HomePage.tsx
@@ -2,10 +2,13 @@
  * @Author: zhangzhong zhong.zhang@duom.com
  * @Date: 2023-04-24 14:03:06
  * @LastEditors: zhangzhong zhong.zhang@duom.com
- * @LastEditTime: 2023-04-24 14:17:03
+ * @LastEditTime: 2023-04-24 14:28:45
  * @FilePath: /stanly/HomePage.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 /// 1 开发首页功能
 /// 2.开发首页功能
 /// 3、开发首页功能
+
+/// 假如你开发的功能home有bug
+/// 1、修改bug
